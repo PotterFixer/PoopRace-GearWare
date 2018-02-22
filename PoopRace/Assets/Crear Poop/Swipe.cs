042459526441d4182eccb3ff492c920ad247cce5
PotterFixer <31879337+PotterFixer@users.noreply.github.com>
2018-02-22 16:15:11 -0600

    feature: inicio crear poop
    inicio para crear poop

---
+++ PoopRace/Assets/Crear Poop/Swipe.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Swipe : MonoBehaviour {
+
+    public GameObject poop;
+
+    private Vector2 startSwipe;
+    private Vector2 endSwipe;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            startSwipe = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            Debug.Log("start swipe: "+startSwipe);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            endSwipe = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            Debug.Log("end swipe: " + endSwipe);
+            SwipeScreen();
+        }
+    }
+
+
+    void SwipeScreen()
+    {
+        poop.transform.localScale += new Vector3(0, 0.05f, 0);
+        Debug.Log(poop.transform.localScale);
+
+        Vector2 swipe = endSwipe - startSwipe;
+
+        
+    }
+}
